b11d146f09f4e8bf3deac4e5e7c9d813e348749f
direct66612 <113045624+direct66612@users.noreply.github.com>
2024-04-26 02:56:04 -0400

    completed createNewPlaceModal ,add icon

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { ref } from "vue";
 import LoginForm from "./components/Auth/LoginForm/LoginForm.vue";
 import RegistrationForm from "./components/Auth/RegistrationForm/RegistrationForm.vue";
-import IModal from "./components/IModal/IModal.vue";
+import CreateNewPlaceModal from "./components/CreateNewPlaceModal/CreateNewPlaceModal.vue";
 
 const isOpen = ref(true);
 
@@ -19,5 +19,9 @@ const openModal = () => {
   <button @click="openModal">Click me</button>
   <RegistrationForm @submit="console.log" />
   <LoginForm @submit="console.log" />
-  <IModal v-if="isOpen" @close="closeModal"> Hello modal </IModal>
+  <CreateNewPlaceModal
+    :is-open="isOpen"
+    @close="closeModal"
+    @submit="console.log"
+  />
 </template>
