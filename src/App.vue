9b780fe3c694ccc5a571c94629f4ee3d47738404
direct66612 <113045624+direct66612@users.noreply.github.com>
2024-04-26 01:18:50 -0400

    add teleport in modal component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script setup>
+import { ref } from "vue";
 import LoginForm from "./components/Auth/LoginForm/LoginForm.vue";
 import RegistrationForm from "./components/Auth/RegistrationForm/RegistrationForm.vue";
 import IModal from "./components/IModal/IModal.vue";
+
+const isOpen = ref(true);
+
+const closeModal = () => {
+  isOpen.value = false;
+};
+
+const openModal = () => {
+  isOpen.value = true;
+};
 </script>
 
 <template>
+  <button @click="openModal">Click me</button>
   <RegistrationForm @submit="console.log" />
   <LoginForm @submit="console.log" />
-  <IModal />
+  <IModal v-if="isOpen" @close="closeModal"> Hello modal </IModal>
 </template>
