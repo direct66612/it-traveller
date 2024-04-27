8099781c43a0478def34b271ed59bdc4220a0bb5
direct66612 <113045624+direct66612@users.noreply.github.com>
2024-04-27 01:24:59 -0400

    add mapbox on homepage , change favoriteplace

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,40 @@
 <script setup>
-import { ref } from "vue";
-import LoginForm from "./components/Auth/LoginForm/LoginForm.vue";
-import RegistrationForm from "./components/Auth/RegistrationForm/RegistrationForm.vue";
-import CreateNewPlaceModal from "./components/CreateNewPlaceModal/CreateNewPlaceModal.vue";
+import { MapboxMap } from "@studiometa/vue-mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { mapSettings } from "./map/settings";
+import FavoritePlaces from "./components/FavoritePlaces/FavoritePlaces.vue";
 
-const isOpen = ref(true);
-
-const closeModal = () => {
-  isOpen.value = false;
-};
-
-const openModal = () => {
-  isOpen.value = true;
-};
+const favoritePlaces = [
+  {
+    id: 1,
+    title: "New place 1",
+    description: "SUper description 1",
+    img: "",
+    lngLat: [],
+  },
+  {
+    id: 2,
+    title: "New place 2",
+    description: "SUper description 2",
+    img: "",
+    lngLat: [],
+  },
+];
 </script>
 
 <template>
-  <button @click="openModal">Click me</button>
-  <RegistrationForm @submit="console.log" />
-  <LoginForm @submit="console.log" />
-  <CreateNewPlaceModal
-    :is-open="isOpen"
-    @close="closeModal"
-    @submit="console.log"
-  />
+  <main class="flex h-screen">
+    <div class="bg-white h-full w-[400px] shrink-0 overflow-auto pb-10">
+      <FavoritePlaces :items="favoritePlaces" />
+    </div>
+    <div class="w-full h-full flex items-center justify-center text-6xl">
+      <MapboxMap
+        class="w-full h-full"
+        :center="[30.523333, 50.450001]"
+        :zoom="10"
+        :access-token="mapSettings.apiToken"
+        :map-style="mapSettings.style"
+      ></MapboxMap>
+    </div>
+  </main>
 </template>
--- src/components/FavoritePlaces/FavoritePlaces.vue
+++ src/components/FavoritePlaces/FavoritePlaces.vue
@@ -1,29 +1,30 @@
 <script setup>
 import FavoritePlace from "../FavoritePlace/FavoritePlace.vue";
 import IButton from "../IButton.vue";
-import IInput from "../IInput/IInput.vue";
-import { ref } from "vue";
 
-const buttonVariant = ref("gradient");
-
-const changeButtonVariant = () => {
-  buttonVariant.value =
-    buttonVariant.value === "gradient" ? "outlined" : "gradient";
-};
+const props = defineProps({
+  items: {
+    required: true,
+    type: Array,
+  },
+});
 </script>
 
 <template>
   <div class="px-6">
     <div class="text-gray mb-4">Додані маркери</div>
-    <div class="py-5">
-      <IInput label="Some label" />
-    </div>
-    <FavoritePlace v-for="n in 4" :key="n" />
-    <IButton
-      class="w-full mt-10"
-      :variant="buttonVariant"
-      @click="changeButtonVariant"
-      >Додати маркер</IButton
-    >
+    <slot name="label"></slot>
+    <slot name="list">
+      <FavoritePlace
+        v-for="place in props.items"
+        :key="place.id"
+        :title="place.title"
+        :description="place.description"
+        :img="place.img"
+      />
+    </slot>
+
+    <slot></slot>
+    <IButton class="w-full mt-10" variant="gradient">Додати маркер</IButton>
   </div>
 </template>
